c1f067be82a630391a53f3102c2abb4ca073aa15
Raj84 <patoliyaraj2222@gmail.com>
2025-07-16 12:59:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    < >
+    <h1 className='w-xl bg-red-200  h-[20vh] text-center sepia  ' >thicdfd</h1>
+    </>
+  );
+}
+
+export default App;
